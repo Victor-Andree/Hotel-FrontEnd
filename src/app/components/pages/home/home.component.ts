138e84de6daa1568a3002bd5b89649bc29b50813
Victor <u21221311@utp.edu.pe>
2025-10-11 18:42:26 -0500

    Agregar ng2-date-picker y actualizar estilos globales; implementar formulario de búsqueda en el componente Home

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { DpDatePickerModule } from 'ng2-date-picker'; 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [DpDatePickerModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+datePickerConfig = {
+  format: 'YYY-MM-DD'
+};
+
 
 }
